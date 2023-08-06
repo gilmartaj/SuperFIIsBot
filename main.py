287e23a2d55f18d2147b6599a79f6b6008e6e6e8
gilmartaj <gilmartaj@gmail.com>
2023-08-05 21:04:01 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ gspread_credentials = {
 
 #print(gspread_credentials)
 
-client = gspread.service_account(filename='superfiisbot-9f67df851d9a.json')
-#client = gspread.service_account_from_dict(gspread_credentials)
+#client = gspread.service_account(filename='superfiisbot-9f67df851d9a.json')
+client = gspread.service_account_from_dict(gspread_credentials)
 
 sheet = client.open("SeguidoresFIIs").sheet1
 sheet_infra = client.open("SeguidoresFI-Infras").sheet1
