eb9eabb3b4ecab6a04f6fd430acfd4140698b6f0
gilmartaj <gilmartaj@gmail.com>
2023-10-07 14:11:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ bot_super = os.getenv("BOT_SUPER_TOKEN")
 TELETHON_API_ID = os.getenv("TELETHON_API_ID")
 TELETHON_API_HASH = os.getenv("TELETHON_API_HASH")
 
-bot = telebot.TeleBot(bot_aux)
+bot = telebot.TeleBot(bot_aux, threaded=False)
 
 app = Flask(__name__)
 
