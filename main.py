58d06e8a5e8cf8c0b7f53170501dcb64e6f14316
gilmartaj <gilmartaj@gmail.com>
2023-11-24 12:13:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1798,11 +1798,11 @@ tz_info = agora().tzinfo
       
 ultima_busca = {}
 for f in base.colunas():
-    ultima_busca[f] = agora() - datetime.timedelta(minutes=90)
+    ultima_busca[f] = agora() - datetime.timedelta(minutes=600)
     
 ultima_busca_infra = {}
 for f in base_infra.colunas():
-    ultima_busca_infra[f] = agora() - datetime.timedelta(minutes=20)
+    ultima_busca_infra[f] = agora() - datetime.timedelta(minutes=600)
     
 def verificar_infra():
     global sheet_infra
